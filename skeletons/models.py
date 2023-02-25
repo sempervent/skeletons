3abe3e977406a33f5461343cc0e3d910e814f2b9
sempervent <jngrant9@gmail.com>
2023-02-25 01:45:47 -0500

    We use Linux for all our mission-critical applications. Having the source code means that we are not held hostage by anyone's support department. (Russell Nelson, President of Crynwr Software)

--- skeletons/models.py
+++ skeletons/models.py
@@ -3,10 +3,27 @@ from os import getenv
 from io import BytesIO, StringIO
 from typing import Union, Optional, Literal
 from pathlib import Path
+from pkg_resources import resource_filename
 
 from pydantic import BaseModel, validator
+from jinja2 import Template
 
 from skeletons.settings import enc
+
+
+class CLXN(BaseModel):
+    name: str
+    lang: Union[str, list]
+    description: Optional[str] = None
+    ext: Union[list, str]
+    tag: Union[list, str]
+
+    @validator('ext', 'lang', 'tag')
+    def _ext_validator(cls, ext: Union[list, str]) -> list:
+        """Always store ext attribute as list."""
+        if isinstance(ext, str):
+            return [ext]
+        return ext
 
 
 class FileModel(BaseModel):
@@ -51,4 +68,59 @@ class FileModel(BaseModel):
             self.path.write_text(data, encoding=self.encoding)
             return None
         return None
+
+
+class ResourceModel(BaseModel):
+    name: str
+    resource: Union[str, Path]
+
+    @validator('resource')
+    def _resource_validator(
+            cls, resource: Union[str, Path]
+    ) -> Path:
+        """Ensure a Path to resource is available."""
+        if isinstance(resource, Path):
+            return resource
+        if isinstance(resource, str):
+            if not resource.endswith('.j2'):
+                resource += '.j2'
+            if not resource.startswith('templates/'):
+                resource = 'templates/' + resource
+            try:
+                resource = Path(resource_filename('skeletons', resource))
+                if resource.is_file():
+                    return resource
+                raise ValueError(
+                        f'{resource} could not be identified as an ' +
+                        'existing file'
+                )
+            except ModuleNotFoundError as uhoh:
+                print('Wow, this is truly an incredible moment.')
+                raise uhoh
+        return Path(resource)
+
+    def parse(self, **kwargs):
+        """Parse the resource with jinja2.
+        Args:
+            kwargs: passed to jinja2.Template.render()
+        """
+        template = Template(self.resource.read_text(**enc))
+        return template.render(**kwargs)
+
+
+class TemplateModel(BaseModel):
+    name: str
+    dest: Union[str, FileModel]
+    resource: Optional[Union[str, ResourceModel]] = None
+
+    @validator('dest')
+    def _dest_validator(
+            cls, dest: Union[str, FileModel], **kwargs
+    ) -> FileModel:
+        """Make sure dest is FileModel.
+        files.
+        """
+        if isisntance(dest, str):
+            return FileModel(path=dest, **kwargs)
+        return dest
 
